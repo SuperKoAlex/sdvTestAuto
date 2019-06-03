a016606f1198e11a5bb2973f0a5550627d61a27c
Alexey Kolobkov <50908095+SuperKoAlex@users.noreply.github.com>
2019-06-03 21:23:30 +0300

    Home Work Number 3

---
+++ Pages/NBCHeader.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace sdvAutomation.Pages
+{
+    public class NBCHeader
+    {
+        public const string lnkShows = "a[href = '/shows']";
+        public const string lnkEpisodes = "a[href = '/video']";
+        public const string lnkSchedule = "a[href = '/schedule']";
+        public const string lnkNewsAndSports = "//span[contains(text(), 'Sports')]/..";
+        public const string lnkShop = "a[href='https://www.nbcstore.com/?cid=nbc_global_nav_shop']";
+        public const string lnkApps = "a[href = '/apps']";
+        public const string lnkSearch = "//button[@class='navigation__search navigation__item__title']";
+        public const string lnkSea = "a[href = '/sign-up']";
+    }
+}
--- Tests/FirtsTest.cs
+++ Tests/FirtsTest.cs
@@ -1,15 +1,27 @@
 ﻿using NUnit.Framework;
+using OpenQA.Selenium.Chrome;
 using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading;
 
 namespace sdvAutomation.Tests
 {
-    class FirstTest
-    {[Test]
+    public class FirstTest
+    {
+        [Test]
         public void FistTest()
-          {
+        {
             Assert.That(1, Is.EqualTo(1));
+        }
+        [Test]
+        public void FirstSeleniumTest()
+        {
+            var driver = new ChromeDriver();
+            driver.Navigate().GoToUrl("http://www.nbc.com");
+            driver.Manage().Window.Maximize();
+            System.Threading.Thread.Sleep(TimeSpan.FromSeconds(5));
+            driver.Quit();
         }
     }
 }
