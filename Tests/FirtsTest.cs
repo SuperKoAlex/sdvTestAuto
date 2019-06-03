dcf79be28a10a59ff71715d474f89eb343fa9489
Alexey Kolobkov <50908095+SuperKoAlex@users.noreply.github.com>
2019-06-03 19:45:29 +0300

    initial commit

---
+++ Tests/FirtsTest.cs
@@ -0,0 +1,15 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace sdvAutomation.Tests
+{
+    class FirstTest
+    {[Test]
+        public void FistTest()
+          {
+            Assert.That(1, Is.EqualTo(1));
+        }
+    }
+}
